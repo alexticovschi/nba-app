f95dd3eca3c304eeda15a98a916f66ee0d5e2ae5
AlexTicovschi <alexmticovschi@gmail.com>
2018-02-12 21:22:33 +0000

    adding NewsList transitions and button component

--- src/components/widgets/NewsList/newsList.js
+++ src/components/widgets/NewsList/newsList.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 
 import { URL } from '../../../config';
 import styles from './newsList.css';
+import Button from '../Buttons/buttons';
+
 
 class NewsList extends Component {
     state = {
@@ -39,13 +41,22 @@ class NewsList extends Component {
         switch(type) {
             case('card'):
                 template = this.state.items.map((item,i) => (
-                    <div>
-                        <div className={styles.newslist_item}>
-                            <Link to={`/articles/${item.id}`}>
-                                <h2>{item.title}</h2>
-                            </Link>
+                    <CSSTransition
+                        classNames={{
+                            enter: styles.newsList_wrapper,
+                            enterActive: styles.newsList_wrapper_enter
+                        }}
+                        timeout={500}
+                        key={i}
+                    >
+                        <div>
+                            <div className={styles.newslist_item}>
+                                <Link to={`/articles/${item.id}`}>
+                                    <h2>{item.title}</h2>
+                                </Link>
+                            </div>
                         </div>
-                    </div>
+                    </CSSTransition>
                 ))
                 break;
             default: 
@@ -57,14 +68,21 @@ class NewsList extends Component {
 
 
     render() {
-        console.log(this.state.items)
+        //console.log(this.props)
 
         return (
             <div>
-                { this.renderNews( this.props.type ) }
-                <button onClick={() => this.loadMore()}>
-                    LOAD MORE
-                </button>
+                <TransitionGroup
+                    component="div"
+                    className="list"
+                >
+                    { this.renderNews( this.props.type ) }
+                </TransitionGroup>
+                <Button 
+                    type="loadmore"
+                    loadMore= {() => this.loadMore()}
+                    cta="Load More News"
+                />
             </div>
         )
     }
