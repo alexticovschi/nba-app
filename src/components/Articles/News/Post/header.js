f2bd5592e4e7347c551f2b660d5a457a1e95a7d7
AlexTicovschi <alexmticovschi@gmail.com>
2018-02-19 21:02:24 +0000

    displaying article header

--- src/components/Articles/News/Post/header.js
+++ src/components/Articles/News/Post/header.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import TeamInfo from '../../Elements/teamInfo';
+import PostData from '../../Elements/postData';
+
+
 const header = (props) => {
+
+    const teamInfo = (team) => {
+        return team ? (
+            <TeamInfo team={team} />
+        ) : null
+    }
+
+    const postData = (date, author) => {
+        return (
+            <div>
+                <PostData data={{date, author}} />
+            </div>
+        )
+    }
+
     return(
         <div>
-            header
+            {teamInfo(props.teamData)}
+            {postData(props.date, props.author)}
         </div>
     )
 }
