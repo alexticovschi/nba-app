3926d46924ce8ecf46a9145402123a8796e285bc
AlexTicovschi <alexmticovschi@gmail.com>
2018-02-22 14:33:13 +0000

    news main view

---
+++ src/components/Articles/News/Main/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import NewsSlider from '../../../widgets/NewsSlider/slider'
+import NewsList from '../../../widgets/NewsList/newsList';
+
+const NewsMain = () => (
+    <div>
+        <NewsSlider
+            type="featured"
+            settings={{dots:false}}
+            start={0}
+            amount={4}
+        />
+        <NewsList
+            type="cardMain"
+            loadMore={true}
+            start={4}
+            amount={4}
+        />
+    </div>
+)
+
+
+export default NewsMain;
